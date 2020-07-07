72ebbd72fd7a13538c3eed18e5a297bf763b4e3f
Akshay Bakshi <apbakshi@hotmail.com>
2020-07-07 20:38:54 +0530

    week 1 completed

--- Algorithmic Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ Algorithmic Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,29 +1,70 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
+long long MaxPairwiseProduct(const std::vector<int>& numbers) {
+    long long max_product = 0;
     int n = numbers.size();
 
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
             max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+                ((long long)numbers[i]) * numbers[j]);
         }
     }
 
     return max_product;
 }
 
+long long MaxPairwiseProductFast(const std::vector<int>& numbers){
+    int n = numbers.size();
+
+    int max_index1 = -1;
+    for (int i = 0; i < n; ++i)
+        if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+            max_index1 = i;
+
+    int max_index2 = -1;
+    for (int j = 0; j < n; ++j)
+        if ((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+            max_index2 = j;
+
+    //std::cout << max_index1 << ' ' << max_index2 << "\n";
+        
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
+}
+
 int main() {
+    /*while(true) {
+        int n = rand() % 1000 + 2;
+        std::cout << n << "\n";
+        std::vector<int> a;
+        for (int i = 0; i < n; ++i){
+            a.push_back(rand() % 100000);
+        }
+        for (int i = 0; i < n; ++i){
+            std::cout << a[i] << ' ';
+        }
+        std::cout << "\n";
+        long long res1 = MaxPairwiseProduct(a);
+        long long res2 = MaxPairwiseProductFast(a);
+        if (res1 != res2) {
+            std::cout << "Wrong answer: " << res1 << ' ' << res2 << "\n";
+            break;
+        }
+        else {
+            std::cout << "OK\n";
+        }
+    }*/
     int n;
     std::cin >> n;
     std::vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i){
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    long long result = MaxPairwiseProductFast(numbers);
+    std::cout << result << "\n";
     return 0;
 }
