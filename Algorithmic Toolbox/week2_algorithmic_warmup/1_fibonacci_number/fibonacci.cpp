566a46b3bc5a690c7e33ea01618e93d70e11430f
Akshay Bakshi <apbakshi@hotmail.com>
2020-07-08 21:41:51 +0530

    fast algo for fibo series

--- Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
+++ Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
@@ -23,8 +23,19 @@ int fibonacci_naive(int n) {
 
 int fibonacci_fast(int n) {
     // write your code here
+    int a = 0;
+    int b = 1;
+    if (n <= 1){
+        return n;
+    }
 
-    return 0;
+    while (n-- > 1) {
+        int t = a;
+        a = b;
+        b += t;
+    }
+    return b;
+    //return 0;
 }
 
 void test_solution() {
@@ -38,8 +49,8 @@ int main() {
     int n = 0;
     std::cin >> n;
 
-    std::cout << fibonacci_naive(n) << '\n';
+    //std::cout << fibonacci_naive(n) << '\n';
     //test_solution();
-    //std::cout << fibonacci_fast(n) << '\n';
+    std::cout << fibonacci_fast(n) << '\n';
     return 0;
 }
--- Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,15 @@
 # Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
-
-    return calc_fib(n - 1) + calc_fib(n - 2)
-
 n = int(input())
-print(calc_fib(n))
+
+if n <= 1:
+    print(n)
+    quit()
+
+def fibo(n):
+    a, b = 0, 1
+    for _ in range(n-1):
+        c = a + b
+        b, a = c, b
+    print(c)
+
+fibo(n)
